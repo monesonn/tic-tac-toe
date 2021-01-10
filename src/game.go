59465a6b1113803baf95cbc3672717b4fd1c3362
monesonn <monesonn@tuta.io>
2021-01-10 19:56:34 +0200

    Commented

--- src/game.go
+++ src/game.go
@@ -1,3 +1,12 @@
+/*
+An implementation of Minimax AI Algorithm in Tic Tac Toe,
+using Golang. For education purpose.
+This program is available under GPL license.
+Author: monesonn
+Year: 2021
+Credit: https://github.com/Cledersonbc/tic-tac-toe-minimax
+	https://github.com/mabsoftware/minimax
+*/
 package main
 
 import (
@@ -8,19 +17,19 @@ import (
 )
 
 const (
-	EMPTY  = 0
-	PLAYER = -1
-	AI     = 1
-	INF    = 2
+	EMPTY  = 0  // empty squares as 0 in array
+	PLAYER = -1 // human squares as -1 in array
+	AI     = 1  // computer squares as 1 in array
+	INF    = 2  // 2 as abstract value of INF, cuz we don't need bigger numbers
 )
 
+// functions to determinate min and max in Minimax
 func min(a, b int) int {
 	if a > b {
 		return b
 	}
 	return a
 }
-
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -28,11 +37,62 @@ func max(a, b int) int {
 	return b
 }
 
-type Strategy struct{ move, payoff int } // struct
+type Strategy struct{ move, payoff int } // struct, packaging move and payoff values
+var clear map[string]func()              // variable for clearScreen function
 
-var clear map[string]func()
+func main() {
+	board := [9]int{}
+	firstMover, numBlanks := 0, 9
+
+	clearScreen()
+	fmt.Printf("Tic-tac-toe\n")
+	fmt.Printf("\twith the minimax algorithm\n")
+	fmt.Printf("Human: X\n")
+	fmt.Printf("Machine: O\n")
+	fmt.Printf("-----------------------------------\n")
+	for i := true; i; i = (firstMover != 1 && firstMover != 2) {
+		fmt.Printf("Would you like to go first or second? [1 / 2] ")
+		fmt.Scanf("%d", &firstMover)
+	}
+	clearScreen()
+	firstMover %= 2
+	for i := 0; i < 9; i++ {
+		if (firstMover+i)%2 == 1 {
+			clearScreen()
+			printBoard(board)
+			getPlayerMove(&board)
+		} else {
+			aiMove := minimax(board, AI, numBlanks).move
+			board[aiMove] = AI
+		}
+		if evaluate(board) != 0 {
+			clearScreen()
+			printBoard(board)
+			if evaluate(board) == AI {
+				fmt.Printf("AI win.\n")
+			} else {
+				fmt.Printf("HUMAN win.\n")
+			}
+			os.Exit(0)
+		}
+		numBlanks--
+	}
+	clearScreen()
+	printBoard(board)
+	fmt.Printf("Draw.\n")
+	os.Exit(0)
+}
 
 func evaluate(board [9]int) int {
+	/*
+		Function to heuristic evaluation of state.
+		Tests if a specific player wins. Possibilities:
+	    * Three rows    [X X X] or [O O O]
+	    * Three cols    [X X X] or [O O O]
+	    * Two diagonals [X X X] or [O O O]
+	    :param board: the state of the current board
+	    :return: +1 if the computer wins; -1 if the human wins; 0 draw
+	*/
 	for i := 0; i < 3; i++ {
 		if board[i] == board[i+3] && board[i] == board[i+6] {
 			return board[i]
@@ -52,28 +112,35 @@ func evaluate(board [9]int) int {
 	return 0
 }
 
-func minimax(board [9]int, whoseTurn, numBlanks int) Strategy {
+func minimax(board [9]int, player, depth int) Strategy {
+	/*
+	   AI function that choice the best move
+	   :param state: current state of the board
+	   :param depth: node index in the tree (0 <= depth <= 9),
+	   :param player: an human or a computer
+	   :return: a structure element {move, payoff}
+	*/
 	evaluation := evaluate(board)
 	if evaluation != 0 {
 		result := Strategy{-1, evaluation}
 		return result
 	}
-	if numBlanks == 0 {
+	if depth == 0 {
 		result := Strategy{-1, 0}
 		return result
 	}
 	bestMove := -1
 	var bestVal int
-	if whoseTurn == AI {
+	if player == AI {
 		bestVal = -INF
 	} else {
 		bestVal = INF
 	}
 	for i := 0; i < 9; i++ {
 		if board[i] == EMPTY {
-			board[i] = whoseTurn
-			branchVal := minimax(board, -whoseTurn, numBlanks-1).payoff
-			if whoseTurn == AI {
+			board[i] = player
+			branchVal := minimax(board, -player, depth-1).payoff
+			if player == AI {
 				bestVal = max(branchVal, bestVal)
 			} else {
 				bestVal = min(branchVal, bestVal)
@@ -90,6 +157,11 @@ func minimax(board [9]int, whoseTurn, numBlanks int) Strategy {
 }
 
 func getSymbol(r int) rune {
+	/*
+		Render character for player for printBoard function
+		:param r: by default array contains representation of players move as [-1:1]
+		:return:
+	*/
 	switch r {
 	case PLAYER:
 		return 'X'
@@ -102,6 +174,11 @@ func getSymbol(r int) rune {
 }
 
 func getPlayerMove(board *[9]int) {
+	/*
+	   The Human plays choosing a valid move.
+	   :param board: changing the current state of the board
+	   :return:
+	*/
 	var move int
 	fmt.Printf("Where would you like to move? [1 - 9] ")
 	fmt.Scanf("%d", &move)
@@ -115,6 +192,10 @@ func getPlayerMove(board *[9]int) {
 }
 
 func printBoard(board [9]int) {
+	/*
+		Print the board on console
+		:param board: current state of the board
+	*/
 	fmt.Printf("\n")
 	fmt.Printf(" %c | %c | %c \n", getSymbol(board[0]), getSymbol(board[1]), getSymbol(board[2]))
 	fmt.Printf("-----------\n")
@@ -123,6 +204,8 @@ func printBoard(board [9]int) {
 	fmt.Printf(" %c | %c | %c \n", getSymbol(board[6]), getSymbol(board[7]), getSymbol(board[8]))
 	fmt.Printf("\n")
 }
+
+// Recognizing system for clear function
 func init() {
 	clear = make(map[string]func())
 	clear["linux"] = func() {
@@ -137,6 +220,7 @@ func init() {
 	}
 }
 
+// Clears the console
 func clearScreen() {
 	value, ok := clear[runtime.GOOS]
 	if ok {
@@ -144,52 +228,4 @@ func clearScreen() {
 	} else {
 		panic("Unsupported platform!") // can't clear terminal
 	}
-}
-
-func main() {
-	board := [9]int{}
-	firstMover, numBlanks := 0, 9
-
-	clearScreen()
-	fmt.Printf("Tic-tac-toe\n")
-	fmt.Printf("\twith the minimax algorithm\n")
-	fmt.Printf("Human: X\n")
-	fmt.Printf("Machine: O\n")
-	fmt.Printf("-----------------------------------\n")
-
-	for i := true; i; i = (firstMover != 1 && firstMover != 2) {
-		fmt.Printf("Would you like to go first or second? [1 / 2] ")
-		fmt.Scanf("%d", &firstMover)
-	}
-
-	clearScreen()
-	firstMover %= 2
-
-	for i := 0; i < 9; i++ {
-		if (firstMover+i)%2 == 1 {
-			clearScreen()
-			printBoard(board)
-			getPlayerMove(&board)
-		} else {
-			aiMove := minimax(board, AI, numBlanks).move
-			board[aiMove] = AI
-		}
-
-		if evaluate(board) != 0 {
-			clearScreen()
-			printBoard(board)
-			if evaluate(board) == AI {
-				fmt.Printf("AI win.\n")
-			} else {
-				fmt.Printf("HUMAN win.\n")
-			}
-			os.Exit(0)
-		}
-		numBlanks--
-	}
-
-	clearScreen()
-	printBoard(board)
-	fmt.Printf("Draw.\n")
-	os.Exit(0)
 }
