eb23dee7e393100b72eae0c8f0632168e5229688
monesonn <monesonn@tuta.io>
2021-01-10 22:39:23 +0200

    Added AB-pruning optimization

--- src/game.go
+++ src/game.go
@@ -25,16 +25,10 @@ const (
 
 // functions to determinate min and max in Minimax
 func min(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
+	if a > b { return b } ; return a
 }
 func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	if a > b { return a } ; return b
 }
 
 type Strategy struct{ move, payoff int } // struct, packaging move and payoff values
@@ -56,13 +50,13 @@ func main() {
 	}
 	clearScreen()
 	firstMover %= 2
-	for i := 0; i < 9; i++ {
+	for i := range board {
 		if (firstMover+i)%2 == 1 {
 			clearScreen()
 			printBoard(board)
 			getPlayerMove(&board)
 		} else {
-			aiMove := minimax(board, AI, numBlanks).move
+			aiMove := minimax(board, numBlanks, -INF, +INF, AI).move
 			board[aiMove] = AI
 		}
 		if evaluate(board) != 0 {
@@ -84,51 +78,36 @@ func main() {
 }
 
 func evaluate(board [9]int) int {
-	/*
-		Function to heuristic evaluation of state.
-		Tests if a specific player wins. Possibilities:
-		* Three rows    [X X X] or [O O O]
-		* Three cols    [X X X] or [O O O]
-		* Two diagonals [X X X] or [O O O]
-		:param board: the state of the current board
-		:return: +1 if the computer wins; -1 if the human wins; 0 draw
-	*/
-	for i := 0; i < 3; i++ {
-		if board[i] == board[i+3] && board[i] == board[i+6] {
-			return board[i]
-		}
-	}
-	for i := 0; i <= 6; i += 3 {
-		if board[i] == board[i+1] && board[i] == board[i+2] {
-			return board[i]
-		}
-	}
-	if board[0] == board[4] && board[0] == board[8] {
-		return board[0]
-	}
-	if board[2] == board[4] && board[2] == board[6] {
-		return board[2]
-	}
+	/* Function to heuristic evaluation of state.
+	Tests if a specific player wins. Possibilities:
+	* Three rows    [X X X] or [O O O]
+	* Three cols    [X X X] or [O O O]
+	* Two diagonals [X X X] or [O O O]
+	:param board: the state of the current board
+	:return: +1 if the computer wins; -1 if the human wins; 0 draw */
+	for i := 0; i < 3; i++ { if board[i] == board[i+3] && board[i] == board[i+6] { return board[i] } } // check columns
+	for i := 0; i <= 6; i += 3 { if board[i] == board[i+1] && board[i] == board[i+2] { return board[i] } } // check rows
+	if board[0] == board[4] && board[0] == board[8] { return board[0] } // check left diagonal
+	if board[2] == board[4] && board[2] == board[6] { return board[2] } // check right diagonal
 	return 0
 }
 
-func minimax(board [9]int, player, depth int) Strategy {
-	/*
-		AI function that choice the best move
-		:param state: current state of the board
-		:param depth: node index in the tree (0 <= depth <= 9),
-		:param player: an human or a computer
-		:return: a structure element {move, payoff}
-	*/
+func minimax(board [9]int, depth, alpha, beta, player int) Strategy {
+	/* AI function that choice the best move
+	:param state: current state of the board
+	:param depth: node index in the tree (0 <= depth <= 9),
+	:param player: an human or a computer
+	:return: a structure element {move, payoff} */
 	evaluation := evaluate(board)
 	if evaluation != 0 {
-		result := Strategy{-1, evaluation}
+		result := Strategy{-1, evaluation} // -1 for the move, since we can't move
 		return result
 	}
 	if depth == 0 {
-		result := Strategy{-1, 0}
+		result := Strategy{-1, 0} // again, -1 for the move because we can't move
 		return result
 	}
+	// Recursive part of the algorithm
 	bestMove := -1
 	var bestVal int
 	if player == AI {
@@ -136,49 +115,53 @@ func minimax(board [9]int, player, depth int) Strategy {
 	} else {
 		bestVal = INF
 	}
-	for i := 0; i < 9; i++ {
+	for i := range board {
 		if board[i] == EMPTY {
-			board[i] = player
-			branchVal := minimax(board, -player, depth-1).payoff
+			// Searching through all branches
+			board[i] = player // player moves
+			branchVal := minimax(board, depth-1, alpha, beta, -player).payoff // 1 and -1 
 			if player == AI {
-				bestVal = max(branchVal, bestVal)
+				bestVal = max(branchVal, bestVal) // the computer is maximizer
+				if bestVal < branchVal {
+					alpha = max(alpha, bestVal)
+					if alpha >= beta {
+						break
+					}
+				}
 			} else {
-				bestVal = min(branchVal, bestVal)
+				bestVal = min(branchVal, bestVal) // the human is minimizer 
+				if bestVal > branchVal {
+					beta = min(beta, bestVal)
+					if beta <= alpha {
+						break
+					}
+				}
 			}
-
-			board[i] = EMPTY
+			board[i] = EMPTY // reset the board
 			if bestVal == branchVal {
-				bestMove = i
+				bestMove = i // keep track of what move yielded the best payoff for player
 			}
 		}
 	}
-	result := Strategy{bestMove, bestVal}
-	return result
+	return Strategy{bestMove, bestVal} // return the best move and payoff associated with it
 }
 
 func getSymbol(r int) rune {
-	/*
-		Render character for player for printBoard function
-		:param r: by default array contains representation of players move as [-1:1]
-		:return:
-	*/
+	/* Render character for player for printBoard function
+	:param r: by default array contains representation of players move as [-1:1]
+	:return: */
 	switch r {
-	case PLAYER:
-		return 'X'
-	case AI:
-		return 'O'
-	case EMPTY:
-		return ' '
+		case PLAYER: return 'X'
+		case AI: return 'O'
+		case EMPTY: return ' '
 	}
 	return '?'
 }
 
 func getPlayerMove(board *[9]int) {
-	/*
-		The Human plays choosing a valid move.
-		:param board: changing the current state of the board
-		:return:
-	*/
+	/* The Human plays choosing a valid move.
+	:param board: changing the current state of the board
+	:return: */
 	var move int
 	fmt.Printf("Where would you like to move? [1 - 9] ")
 	fmt.Scanf("%d", &move)
@@ -192,10 +175,8 @@ func getPlayerMove(board *[9]int) {
 }
 
 func printBoard(board [9]int) {
-	/*
-		Print the board on console
-		:param board: current state of the board
-	*/
+	/* Print the board on console
+	:param board: current state of the board */
 	fmt.Printf("\n")
 	fmt.Printf(" %c | %c | %c \n", getSymbol(board[0]), getSymbol(board[1]), getSymbol(board[2]))
 	fmt.Printf("-----------\n")
